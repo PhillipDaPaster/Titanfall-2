bdd986a9d73db8174e7492d9937da3ce137748f2
Phillip <49299203+PhillipDaPaster@users.noreply.github.com>
2025-09-22 01:12:20 -0400

    Create CamThink.h

---
+++ Client/CamThink.h
@@ -0,0 +1,11 @@
+struct CameraState {
+    uint8_t flag1;   // 0x74
+    uint8_t flag2;   // 0x75
+    uint8_t flag3;   // 0x76
+    float pitch;     // 0x78
+    float yaw;       // 0x7C
+    float dist;      // 0x80
+    uint8_t flag4;   // 0x84
+    int value1;      // 0x90
+    int value2;      // 0x94
+};
